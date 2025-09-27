0bc6312f8f18a5102aa6c5de17a1a7533ff695a7
Ellena Jiang <ellenajiang28@gmail.com>
2025-09-27 14:44:06 -0400

    Run new simulations with non-monotonousness fix

--- players/player_2/process_json.py
+++ players/player_2/process_json.py
@@ -4,21 +4,36 @@ import json
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Parse a JSON file and extract specific keys.')
 	parser.add_argument('--file', '-f', required=True, help='Path to the JSON file to parse.')
-	parser.add_argument('--length', '-l', type=int)
+	parser.add_argument('--sim_num', '-s', type=int)
 	parser.add_argument('--players', '-p', type=int)
 	args = parser.parse_args()
 
 	with open(args.file) as f:
 		data = json.load(f)
 
-	for turn in range(args.length):
-		turn_data = data['turn_impact'][turn]
-		if turn_data['speaker_name'] == 'Player2':
-			player_id = turn_data['speaker_id']
+	# player_ids = set()
+	# for turn in range(args.length):
+	# 	turn_data = data['turn_impact'][turn]
+	# 	if turn_data['speaker_name'] == 'Player2':
+	# 		player_id = turn_data['speaker_id']
+	# 		player_ids.add(player_id)
 
+	# assert len(player_ids) == args.players
+
+	shared_scores = data['score_breakdown']
+
+	# Individual scores
 	for p in range(args.players):
 		curr_player_id = data['scores']['player_scores'][p]['id']
-		if curr_player_id == player_id:
-			our_final_score = data['scores']['player_scores'][p]['scores']
 
-	print(our_final_score['total'], our_final_score['shared'], our_final_score['individual'])
+		final_score = data['scores']['player_scores'][p]['scores']
+
+		print(f"{args.sim_num}, {p}, {final_score['total']}, {final_score['individual']}, {shared_scores['total']}, {shared_scores['importance']}, {shared_scores['coherence']}, {shared_scores['freshness']}, {shared_scores['nonmonotonousness']}")
+
+#"score_breakdown": {
+#     "total": 166.74000000000004,
+#     "importance": 96.74000000000004,
+#     "coherence": 67.0,
+#     "freshness": 8.0,
+#     "nonmonotonousness": -5.0
+#   },
--- players/player_2/run_simulation.sh
+++ players/player_2/run_simulation.sh
@@ -7,16 +7,20 @@ sim_num=$5
 tmp_json_file=$6
 csv_file=$7
 
+echo "sim_num,player,player_total,player_individual,total_shared, total_importance, total_coherence, total_freshness, total_nonmonotonousness" > $csv_file
 for ((i=1; i<=sim_num; i++)); do
     echo "Simulation $i"
     # Run simulation
     touch $tmp_json_file
-    uv run python main.py --player p2 1 --player pr 2 --length $length --subjects $subjects --memory_size $memory_size  --seed $i > $tmp_json_file
+    uv run python main.py --player p2 5 --length $length --subjects $subjects --memory_size $memory_size  --seed $i > $tmp_json_file
 
     # Delete first two line
     sed -i '' '1,2d' $tmp_json_file
 
     # Process JSON
-    echo "total,shared,individual" >> $csv_file
-    python players/player_2/process_json.py --file $tmp_json_file --length $length --players $players_num >> $csv_file
+    python players/player_2/process_json.py --file $tmp_json_file --sim_num $i --players $players_num >> $csv_file
 done
+
+# bash players/player_2/run_simulation.sh 200 5 20 5 10 tmp.json results_mem_not_enough.csv
+# bash players/player_2/run_simulation.sh 200 5 40 5 10 tmp.json results_mem_just_enough.csv
+# bash players/player_2/run_simulation.sh 200 5 60 5 10 tmp.json results_mem_more_than_enough.csv
